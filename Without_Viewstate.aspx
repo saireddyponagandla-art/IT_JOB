1a2831bf3fe1eddea40d3b2cba8bc08ef7bb3f75
saire <saire@SAIREDDY_2005>
2026-04-21 15:38:55 +0530

    new code

---
+++ Without_Viewstate.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Without_Viewstate.aspx.cs" Inherits="IT_JOB.Without_Viewstate" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="label1" runat="server" Text="Label"></asp:Label>
+<asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Without_Viewstate.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace IT_JOB
+{
+    public partial class Without_Viewstate : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                {
+                    ViewState["Counter"] = 0;
+                }
+
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            count++;
+            label1.Text = "Button clicked " + count + " times.";
+        }
+    }
+
+    }
